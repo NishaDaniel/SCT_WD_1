9bae432f6a70b5c45da75f006a7f9144f6b1b524
NishaDaniel <146612044+NishaDaniel@users.noreply.github.com>
2024-08-30 21:44:06 +0530

    app.js

---
+++ Navigation/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.main-head');
+    if (window.scrollY > 0) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
